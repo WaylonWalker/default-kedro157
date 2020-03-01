4bbb5e5cde0d8c34dcb64e897e1f171cf4b6aacc
WaylonWalker <walker_waylon_s@cat.com>
2020-02-29 19:48:59 -0600

    attempting to fix window not available on build

--- gatsby-kedro-viz/src/pages/index.js
+++ gatsby-kedro-viz/src/pages/index.js
@@ -7,14 +7,28 @@ import SEO from "../components/seo"
 import data from './default-kedro157.json'
 import KedroViz from '@quantumblack/kedro-viz';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>default kedro pipeline</h1>
-    <div className="pipeline" style={{ minHeight: '80vh' }}>
-      <KedroViz style={{ minHeight: '80vh' }} data={data} />
-    </div>
-  </Layout>
-)
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loaded: false
+    }
+    this.componentDidMount = () => {
+      this.setState({ loaded: false })
+
+    }
+  }
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" />
+        <h1>default kedro pipeline</h1>
+        <div className="pipeline" style={{ minHeight: '80vh' }}>
+          {this.state.loaded === false ? 'loading' : <KedroViz style={{ minHeight: '80vh' }} data={data} />}
+        </div>
+      </Layout>
+    )
+  }
+}
 
 export default IndexPage
