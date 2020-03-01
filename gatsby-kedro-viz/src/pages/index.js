8bea5ab5fef6ba4c2d252614168c7070d2bb09e3
WaylonWalker <walker_waylon_s@cat.com>
2020-03-01 10:53:29 -0600

    CLEAN removed unused imports

--- gatsby-kedro-viz/src/pages/index.js
+++ gatsby-kedro-viz/src/pages/index.js
@@ -1,8 +1,6 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 import data from './default-kedro157.json'
 import KedroViz from '@quantumblack/kedro-viz';
@@ -15,14 +13,12 @@ class IndexPage extends React.Component {
     }
     this.componentDidMount = () => {
       this.setState({ loaded: true })
-
     }
   }
   render() {
     return (
       <Layout>
         <SEO title="Home" />
-        {/* <h1>default kedro pipeline</h1> */}
         <div className="pipeline" style={{ minHeight: '80vh' }}>
           {this.state.loaded === false ? 'loading' : <KedroViz style={{ height: '80vh' }} data={data} />}
         </div>
