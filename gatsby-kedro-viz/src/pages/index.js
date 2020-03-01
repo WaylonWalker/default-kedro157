aab8c3c1e8edcb6942fd42c4f40a970f3c9362dd
WaylonWalker <walker_waylon_s@cat.com>
2020-03-01 09:12:53 -0600

    FIX removed redundant title

--- gatsby-kedro-viz/src/pages/index.js
+++ gatsby-kedro-viz/src/pages/index.js
@@ -22,7 +22,7 @@ class IndexPage extends React.Component {
     return (
       <Layout>
         <SEO title="Home" />
-        <h1>default kedro pipeline</h1>
+        {/* <h1>default kedro pipeline</h1> */}
         <div className="pipeline" style={{ minHeight: '80vh' }}>
           {this.state.loaded === false ? 'loading' : <KedroViz style={{ height: '80vh' }} data={data} />}
         </div>
